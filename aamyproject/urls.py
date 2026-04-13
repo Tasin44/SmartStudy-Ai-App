779896cfba05531264123f333fd5d599dc3fde71
Tasin44 <tasinmahmud44@gmail.com>
2026-04-13 12:43:55 +0600

    image preview issue solved

--- aamyproject/urls.py
+++ aamyproject/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +28,5 @@ urlpatterns = [
     path('library/', include('libraryapp.urls')),
     path('2fa/', include('twofapp.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
